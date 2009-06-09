3336542014f69238e65d902251428122f3bc2343
Ígor Bonadio <igorbonadio@gmail.com>
2009-06-09 18:45:25 -0300

    Teste do camshift basico em main.c, porem existe algum erro de alocação de memória, pois ela cresce constantemente...

--- src/main.c
+++ src/main.c
@@ -1,236 +1,184 @@
-//#ifdef _CH_
-//#pragma package <opencv>
-//#endif
-//
-//#ifndef _EiC
-//#include "cv.h"
-//#include "highgui.h"
-//#include <stdio.h>
-//#include <ctype.h>
-//#endif
-//
-//#include "driver/Camshift.h"
-//
-//IplImage *image = 0, *hsv = 0, *hue = 0, *mask = 0, *backproject = 0, *histimg = 0;
-//CvHistogram *hist = 0;
-//
-//int backproject_mode = 0;
-//int select_object = 0;
-//int track_object = 0;
-//int show_hist = 1;
-//CvPoint origin;
-//CvRect selection;
-//CvRect track_window;
-//CvBox2D track_box;
-//CvConnectedComp track_comp;
-//int hdims = 16;
-//float hranges_arr[] = {0,180};
-//float* hranges = hranges_arr;
-//int vmin = 10, vmax = 256, smin = 30;
-//
-//void on_mouse( int event, int x, int y, int flags, void* param )
-//{
-//    if( !image )
-//        return;
-//
-//    if( image->origin )
-//        y = image->height - y;
-//
-//    if( select_object )
-//    {
-//        selection.x = MIN(x,origin.x);
-//        selection.y = MIN(y,origin.y);
-//        selection.width = selection.x + CV_IABS(x - origin.x);
-//        selection.height = selection.y + CV_IABS(y - origin.y);
-//        
-//        selection.x = MAX( selection.x, 0 );
-//        selection.y = MAX( selection.y, 0 );
-//        selection.width = MIN( selection.width, image->width );
-//        selection.height = MIN( selection.height, image->height );
-//        selection.width -= selection.x;
-//        selection.height -= selection.y;
-//    }
-//
-//    switch( event )
-//    {
-//    case CV_EVENT_LBUTTONDOWN:
-//        origin = cvPoint(x,y);
-//        selection = cvRect(x,y,0,0);
-//        select_object = 1;
-//        break;
-//    case CV_EVENT_LBUTTONUP:
-//        select_object = 0;
-//        if( selection.width > 0 && selection.height > 0 )
-//            track_object = -1;
-//        break;
-//    }
-//}
-//
-//
-//CvScalar hsv2rgb( float hue )
-//{
-//    int rgb[3], p, sector;
-//    static const int sector_data[][3]=
-//        {{0,2,1}, {1,2,0}, {1,0,2}, {2,0,1}, {2,1,0}, {0,1,2}};
-//    hue *= 0.033333333333333333333333333333333f;
-//    sector = cvFloor(hue);
-//    p = cvRound(255*(hue - sector));
-//    p ^= sector & 1 ? 255 : 0;
-//
-//    rgb[sector_data[sector][0]] = 255;
-//    rgb[sector_data[sector][1]] = 0;
-//    rgb[sector_data[sector][2]] = p;
-//
-//    return cvScalar(rgb[2], rgb[1], rgb[0],0);
-//}
-//
-//int main( int argc, char** argv )
-//{
-//    CvCapture* capture = 0;
-//    
-//    if( argc == 1 || (argc == 2 && strlen(argv[1]) == 1 && isdigit(argv[1][0])))
-//        capture = cvCaptureFromCAM( argc == 2 ? argv[1][0] - '0' : 0 );
-//    else if( argc == 2 )
-//        capture = cvCaptureFromAVI( argv[1] ); 
-//
-//    if( !capture )
-//    {
-//        fprintf(stderr,"Could not initialize capturing...\n");
-//        return -1;
-//    }
-//
-//    printf( "Hot keys: \n"
-//        "\tESC - quit the program\n"
-//        "\tc - stop the tracking\n"
-//        "\tb - switch to/from backprojection view\n"
-//        "\th - show/hide object histogram\n"
-//        "To initialize tracking, select the object with mouse\n" );
-//
-//    cvNamedWindow( "Histogram", 1 );
-//    cvNamedWindow( "CamShiftDemo", 1 );
-//    cvSetMouseCallback( "CamShiftDemo", on_mouse, 0 );
-//    cvCreateTrackbar( "Vmin", "CamShiftDemo", &vmin, 256, 0 );
-//    cvCreateTrackbar( "Vmax", "CamShiftDemo", &vmax, 256, 0 );
-//    cvCreateTrackbar( "Smin", "CamShiftDemo", &smin, 256, 0 );
-//
-//    for(;;)
-//    {
-//        IplImage* frame = 0;
-//        int i, bin_w, c;
-//
-//        frame = cvQueryFrame( capture );
-//        if( !frame )
-//            break;
-//
-//        if( !image )
-//        {
-//            /* allocate all the buffers */
-//            image = cvCreateImage( cvGetSize(frame), 8, 3 );
-//            image->origin = frame->origin;
-//            hsv = cvCreateImage( cvGetSize(frame), 8, 3 );
-//            hue = cvCreateImage( cvGetSize(frame), 8, 1 );
-//            mask = cvCreateImage( cvGetSize(frame), 8, 1 );
-//            backproject = cvCreateImage( cvGetSize(frame), 8, 1 );
-//            hist = cvCreateHist( 1, &hdims, CV_HIST_ARRAY, &hranges, 1 );
-//            histimg = cvCreateImage( cvSize(320,200), 8, 3 );
-//            cvZero( histimg );
-//        }
-//
-//        cvCopy( frame, image, 0 );
-//        cvCvtColor( image, hsv, CV_BGR2HSV );
-//
-//        if( track_object )
-//        {
-//            int _vmin = vmin, _vmax = vmax;
-//
-//            cvInRangeS( hsv, cvScalar(0,smin,MIN(_vmin,_vmax),0),
-//                        cvScalar(180,256,MAX(_vmin,_vmax),0), mask );
-//            cvSplit( hsv, hue, 0, 0, 0 );
-//
-//            if( track_object < 0 )
-//            {
-//                hist = getHistogram(hue, mask, selection);
-//                track_window = selection;
-//                track_object = 1;
-//            }
-//
-//			track_window = camshift(hue, mask, track_window, hist, &track_box);
-//            
-//            if( backproject_mode )
-//                cvCvtColor( backproject, image, CV_GRAY2BGR );
-//            if( !image->origin )
-//                track_box.angle = -track_box.angle;
-//            cvEllipseBox( image, track_box, CV_RGB(255,0,0), 3, CV_AA, 0 );
-//        }
-//        
-//        if( select_object && selection.width > 0 && selection.height > 0 )
-//        {
-//            cvSetImageROI( image, selection );
-//            cvXorS( image, cvScalarAll(255), image, 0 );
-//            cvResetImageROI( image );
-//        }
-//
-//        cvShowImage( "CamShiftDemo", image );
-//        cvShowImage( "Histogram", histimg );
-//
-//        c = cvWaitKey(10);
-//        if( (char) c == 27 ){
-//            //cvSaveImage("image.bmp",frame);
-//			//cvSaveImage("image2.bmp", image);
-//			//cvSaveImage("image3.bmp", backproject);
-//			break;
-//		}
-//        switch( (char) c )
-//        {
-//        case 'b':
-//            backproject_mode ^= 1;
-//            break;
-//        case 'c':
-//            track_object = 0;
-//            cvZero( histimg );
-//            break;
-//        case 'h':
-//            show_hist ^= 1;
-//            if( !show_hist )
-//                cvDestroyWindow( "Histogram" );
-//            else
-//                cvNamedWindow( "Histogram", 1 );
-//            break;
-//        default:
-//            ;
-//        }
-//    }
-//
-//    cvReleaseCapture( &capture );
-//    cvDestroyWindow("CamShiftDemo");
-//
-//    return 0;
-//}
-//
-//#ifdef _EiC
-//main(1,"camshiftdemo.c");
-//#endif
-//
-
 #include "driver/Camshift.h"
-#include <stdio.h>
 
-int main(){
-	MarkerList list;
-	list = NULL;
-	CvRect p;
-	CvBox2D t;
-	addMarker(&list, 1, p, t, NULL);
-	addMarker(&list, 2, p, t, NULL);
-	addMarker(&list, 3, p, t, NULL);
-	
-	removeAllMarkers(&list);
-	
-	Marker* m;
-	m = list;
-	while(m != NULL){
-		printf("%d\n", m->id);
-		m = m->next;
-	}
-	
+#ifdef _CH_
+#pragma package <opencv>
+#endif
+
+#ifndef _EiC
+#include "cv.h"
+#include "highgui.h"
+#include <stdio.h>
+#include <ctype.h>
+#endif
+
+MarkerList markerList;
+
+IplImage *image = 0, *hsv = 0, *hue = 0, *mask = 0, *backproject = 0, *histimg = 0;
+CvHistogram *hist = 0;
+
+int backproject_mode = 0;
+int select_object = 0;
+int track_object = 0;
+int show_hist = 1;
+CvPoint origin;
+CvRect selection;
+CvRect track_window;
+CvBox2D track_box;
+CvConnectedComp track_comp;
+int hdims = 16;
+float hranges_arr[] = {0,180};
+float* hranges = hranges_arr;
+int vmin = 10, vmax = 256, smin = 30;
+
+void on_mouse( int event, int x, int y, int flags, void* param )
+{
+    if( !image )
+        return;
+
+    if( image->origin )
+        y = image->height - y;
+
+    if( select_object )
+    {
+        selection.x = MIN(x,origin.x);
+        selection.y = MIN(y,origin.y);
+        selection.width = selection.x + CV_IABS(x - origin.x);
+        selection.height = selection.y + CV_IABS(y - origin.y);
+        
+        selection.x = MAX( selection.x, 0 );
+        selection.y = MAX( selection.y, 0 );
+        selection.width = MIN( selection.width, image->width );
+        selection.height = MIN( selection.height, image->height );
+        selection.width -= selection.x;
+        selection.height -= selection.y;
+    }
+
+    switch( event )
+    {
+    case CV_EVENT_LBUTTONDOWN:
+        origin = cvPoint(x,y);
+        selection = cvRect(x,y,0,0);
+        select_object = 1;
+        break;
+    case CV_EVENT_LBUTTONUP:
+        select_object = 0;
+        if( selection.width > 0 && selection.height > 0 )
+            track_object = -1;
+        break;
+    }
 }
+
+int main( int argc, char** argv )
+{
+	markerList = NULL;
+	int id = 0;
+	
+    CvCapture* capture = 0;
+    
+    if( argc == 1 || (argc == 2 && strlen(argv[1]) == 1 && isdigit(argv[1][0])))
+        capture = cvCaptureFromCAM( argc == 2 ? argv[1][0] - '0' : 0 );
+    else if( argc == 2 )
+        capture = cvCaptureFromAVI( argv[1] ); 
+
+    if( !capture )
+    {
+        fprintf(stderr,"Could not initialize capturing...\n");
+        return -1;
+    }
+
+    printf( "Hot keys: \n"
+        "\tESC - quit the program\n"
+        "\tc - stop the tracking\n"
+        "\tb - switch to/from backprojection view\n"
+        "\th - show/hide object histogram\n"
+        "To initialize tracking, select the object with mouse\n" );
+
+    cvNamedWindow( "Histogram", 1 );
+    cvNamedWindow( "CamShiftDemo", 1 );
+    cvSetMouseCallback( "CamShiftDemo", on_mouse, 0 );
+    cvCreateTrackbar( "Vmin", "CamShiftDemo", &vmin, 256, 0 );
+    cvCreateTrackbar( "Vmax", "CamShiftDemo", &vmax, 256, 0 );
+    cvCreateTrackbar( "Smin", "CamShiftDemo", &smin, 256, 0 );
+
+    for(;;)
+    {
+        IplImage* frame = 0;
+        int i, bin_w, c;
+
+        frame = cvQueryFrame( capture );
+        if( !frame )
+            break;
+
+        if( !image )
+        {
+            /* allocate all the buffers */
+            image = cvCreateImage( cvGetSize(frame), 8, 3 );
+            image->origin = frame->origin;
+            hsv = cvCreateImage( cvGetSize(frame), 8, 3 );
+            hue = cvCreateImage( cvGetSize(frame), 8, 1 );
+            mask = cvCreateImage( cvGetSize(frame), 8, 1 );
+            backproject = cvCreateImage( cvGetSize(frame), 8, 1 );
+            hist = cvCreateHist( 1, &hdims, CV_HIST_ARRAY, &hranges, 1 );
+            histimg = cvCreateImage( cvSize(320,200), 8, 3 );
+            cvZero( histimg );
+        }
+
+        cvCopy( frame, image, 0 );
+        cvCvtColor( image, hsv, CV_BGR2HSV );
+
+        if( track_object )
+        {
+            int _vmin = vmin, _vmax = vmax;
+
+            cvInRangeS( hsv, cvScalar(0,smin,MIN(_vmin,_vmax),0),
+                        cvScalar(180,256,MAX(_vmin,_vmax),0), mask );
+            cvSplit( hsv, hue, 0, 0, 0 );
+
+            if( track_object < 0 )
+            {
+                track_object = 1;
+
+				CvBox2D track_box;
+				addMarker(&markerList, id, selection, track_box, getHistogram(hue, mask, selection) );
+				id++;
+            }
+
+			CvBox2D tb;
+			CvRect s1l;
+			Marker* marker = markerList;
+			while(marker != NULL){
+				marker->position = camshift(hue, mask, marker->position, marker->hist, &(marker->track_box));
+				
+				//if( !image->origin )
+				//	marker->track_box.angle = -(marker->track_box.angle);
+				//cvEllipseBox( image, marker->track_box, CV_RGB(255,0,0), 3, CV_AA, 0 );
+				
+				marker = marker->next;
+			}
+        }
+        
+        if( select_object && selection.width > 0 && selection.height > 0 )
+        {
+            cvSetImageROI( image, selection );
+            cvXorS( image, cvScalarAll(255), image, 0 );
+            cvResetImageROI( image );
+        }
+
+        cvShowImage( "CamShiftDemo", image );
+        cvShowImage( "Histogram", histimg );
+
+        c = cvWaitKey(10);
+        if( (char) c == 27 ){
+			break;
+		}
+    }
+
+    cvReleaseCapture( &capture );
+    cvDestroyWindow("CamShiftDemo");
+
+    return 0;
+}
+
+#ifdef _EiC
+main(1,"camshiftdemo.c");
+#endif
